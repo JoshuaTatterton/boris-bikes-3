378ed5f2f0f4e2eaf4cd5843325e9158bf5c4a06
ajitsy <sanghera.p@gmail.com>
2015-07-23 12:06:23 +0100

    Written unit test for release_bike if broken

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -21,11 +21,21 @@ end
     end
   end
 
+  describe '#release_bike' do
+    it 'raises an error when trying to release broken bike' do
+      bike = Bike.new
+      bike.report_broken
+      subject.dock bike
+      expect { subject.release_bike}.to raise_error 'No bikes available'
+    end
+  end
+
   describe '#dock' do
    it 'raises an error when full' do
      20.times { subject.dock Bike.new }
      expect { subject.dock Bike.new }.to raise_error 'Docking station full'
    end
- end
+
+end
 
 end
